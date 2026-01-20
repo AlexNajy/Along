80e21fe9f3b6a6fbed1eb11ee5e05c8e76bd0340
Alexander Najy <Alexander.najy@gmail.com>
2026-01-19 18:47:48 -0800

    setup/test configs for nativewind (incomplete)

--- app/(roots)/(tabs)/map.tsx
+++ app/(roots)/(tabs)/map.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import {Text, View} from "react-native"
+import { Text, View } from "react-native"
 
 const Map = () => {
     return (
-        <View>
-            <Text>Map Page</Text>
+        <View className="flex-1 items-center justify-center bg-red-600">
+            <Text className="text-xl font-bold text-blue-500">
+                Welcome to Nativewind!
+            </Text>
         </View>
     )
 }
