5541ca555fda70ce7592d9edfc95f9df364325e9
Alexander Najy <Alexander.najy@gmail.com>
2026-01-20 19:41:31 -0800

    created signing page template

--- app/(roots)/(tabs)/map.tsx
+++ app/(roots)/(tabs)/map.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native"
 const Map = () => {
     return (
         <View>
-            <Text className="font-Bold my-10 font-rubik text-3xl">This is a test for the rubik font -alex is pussylips at coding- </Text>
+            <Text className="font-Bold my-10 font-rubik text-3xl">This is a test for the rubik font</Text>
         </View>
     )
 }
