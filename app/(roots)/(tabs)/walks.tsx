315d17b5ab9a4c970d52b1e0147737f2d2a37b91
Alexander Najy <Alexander.najy@gmail.com>
2026-01-21 21:16:36 -0800

    demo list renders with warnings

--- app/(roots)/(tabs)/walks.tsx
+++ app/(roots)/(tabs)/walks.tsx
@@ -1,12 +1,41 @@
-import React from "react";
-import {Text, View} from "react-native"
+import React, { useState, useEffect } from "react";
+import { Text, View, FlatList } from "react-native"
+import { supabase } from "@/libs/supabase";
 
 const Walks = () => {
+    const [names, setNames] = useState([]);
+
+    useEffect(() => {
+        const getNames = async () => {
+            try {
+                const { data: names, error } = await supabase.from('demo').select('*');
+
+                if (error) {
+                    console.error('Error fetching names:', error.message);
+                    return;
+                }
+
+                if (names && names.length > 0) {
+                    setNames(names);
+                }
+            } catch (error) {
+                console.error('Error fetching names:', error.message);
+            }
+        };
+
+        getNames();
+
+    }, []);
     return (
-        <View>
-            <Text>Walks Page</Text>
+        <View className="flex-1 justify-center items-center">
+          <Text className="font-rubikBold">Names</Text>
+          <FlatList
+            data={names}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => <Text key={item.id}>{item.name}</Text>}
+          />
         </View>
-    )
+      );
 }
 
 export default Walks
