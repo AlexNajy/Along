95c47514252e96089990e8f837e3265e23e36c10
Alexander Najy <Alexander.najy@gmail.com>
2026-01-28 00:58:00 -0800

    styled the map component

--- app/(roots)/(tabs)/map.tsx
+++ app/(roots)/(tabs)/map.tsx
@@ -1,48 +1,62 @@
 import React from "react";
 import { router } from "expo-router";
-import { Text, View, Pressable, StyleSheet} from "react-native";
+import { Text, View, Pressable, StyleSheet } from "react-native";
 import Mapbox from '@rnmapbox/maps';
 
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN!);
 
 const Map = () => {
     return (
-        <View className="flex-1 items-center justify-center bg-background">
-
+        <View style={styles.container}>
             <Mapbox.MapView
                 style={styles.map}
-                logoEnabled={true} // legally required don't change 
-                attributionEnabled={true} // legally required don't change 
-                styleURL={Mapbox.StyleURL.Street}
+                // logoEnabled={true} // legally required don't change
+                // attributionEnabled={true} // legally required don't change
+                styleURL={Mapbox.StyleURL.TrafficNight}
+                scaleBarEnabled={false}
             >
                 <Mapbox.Camera
+                    minZoomLevel={12}
+                    maxZoomLevel={18}
                     zoomLevel={14}
                     centerCoordinate={[-123.2460, 49.2606]}
                     animationMode="flyTo"
                     animationDuration={2000}
+                    maxBounds={{
+                        ne: [-123.2254, 49.2825],
+                        sw: [-123.2676, 49.2437],
+                    }}
                 />
             </Mapbox.MapView>
 
 
             <Pressable
                 onPress={() => router.push("/(roots)/create_walks")}
-                className="h-14 px-8 items-center justify-center bg-primary-300 rounded-2xl mt-6"
-            >
+                style={styles.button}
+                className="h-14 items-center justify-center rounded-2xl bg-primary-300" >
                 <Text className="text-lg font-rubikMedium text-white">
                     Create a walk
                 </Text>
             </Pressable>
-
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
     map: {
         flex: 1,
         width: "100%",
+    },
+    button: {
+        position: "absolute",
+        bottom: 30,
+        alignSelf: "center",
+        width: 200,
+        height: 56,
     }
 });
 
 export default Map;
-
