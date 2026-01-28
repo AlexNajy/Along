ffb9e509ecba67d3f8e6e8973a74f176f7dbf2ee
Alexander Najy <Alexander.najy@gmail.com>
2026-01-27 23:08:17 -0800

    installed mapbox

--- app/(roots)/(tabs)/map.tsx
+++ app/(roots)/(tabs)/map.tsx
@@ -3,20 +3,21 @@ import { router } from "expo-router";
 import { Text, View, Pressable } from "react-native";
 
 const Map = () => {
-  return (
-    <View className="flex-1 items-center justify-center bg-background">
-      <Text className="text-2xl font-rubikBold text-black-300">Map</Text>
+    return (
+        <View className="flex-1 items-center justify-center bg-background">
+            <Text className="text-2xl font-rubikBold text-black-300">Map</Text>
 
-      <Pressable
-        onPress={() => router.push("/(roots)/create_walks")}
-        className="h-14 px-8 items-center justify-center bg-primary-300 rounded-2xl mt-6"
-      >
-        <Text className="text-lg font-rubikMedium text-white">
-          Create a walk
-        </Text>
-      </Pressable>
-    </View>
-  );
+            <Pressable
+                onPress={() => router.push("/(roots)/create_walks")}
+                className="h-14 px-8 items-center justify-center bg-primary-300 rounded-2xl mt-6"
+            >
+                <Text className="text-lg font-rubikMedium text-white">
+                    Create a walk
+                </Text>
+            </Pressable>
+
+        </View>
+    );
 };
 
 export default Map;
