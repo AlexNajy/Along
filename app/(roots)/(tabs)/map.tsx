c4d2ad273997098a76302483a3d807b15274302e
Alexander Najy <Alexander.najy@gmail.com>
2026-01-28 11:42:14 -0800

    added building extrusion

--- app/(roots)/(tabs)/map.tsx
+++ app/(roots)/(tabs)/map.tsx
@@ -12,9 +12,11 @@ const Map = () => {
                 style={styles.map}
                 // logoEnabled={true} // legally required don't change
                 // attributionEnabled={true} // legally required don't change
+                attributionPosition={{ top: 5, left: 8 }}
                 styleURL={Mapbox.StyleURL.TrafficNight}
                 scaleBarEnabled={false}
             >
+
                 <Mapbox.Camera
                     minZoomLevel={12}
                     maxZoomLevel={18}
@@ -26,7 +28,26 @@ const Map = () => {
                         ne: [-123.2254, 49.2825],
                         sw: [-123.2676, 49.2437],
                     }}
+                    pitch={30}
                 />
+
+                <Mapbox.VectorSource id="mapbox-buildings" url="mapbox://mapbox.mapbox-streets-v8">
+                    <Mapbox.VectorSource id="mapbox-buildings" url="mapbox://mapbox.mapbox-streets-v8">
+                        <Mapbox.FillExtrusionLayer
+                            id="3d-buildings"
+                            sourceLayerID="building"
+                            minZoomLevel={12}  // required
+                            maxZoomLevel={22}  // required
+                            style={{
+                                fillExtrusionHeight: ['get', 'height'],
+                                fillExtrusionBase: ['get', 'min_height'],
+                                fillExtrusionColor: '#aaa',
+                                fillExtrusionOpacity: 0.6,
+                            }}
+                        />
+                    </Mapbox.VectorSource>
+
+                </Mapbox.VectorSource>
             </Mapbox.MapView>
 
 
