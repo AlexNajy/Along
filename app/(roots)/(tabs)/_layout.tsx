70ba5cd5fedabaa2934fb6b19d03955ec5eafa59
Alexander Najy <Alexander.najy@gmail.com>
2026-02-01 14:07:16 -0800

    wrapped components in safeAreaView

--- app/(roots)/(tabs)/_layout.tsx
+++ app/(roots)/(tabs)/_layout.tsx
@@ -1,62 +1,60 @@
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
-import { ThemeProvider, useTheme } from "@/context/ThemeContext";
+import { useTheme } from "@/context/ThemeContext";
 
 function TabsLayout() {
-  const { colors } = useTheme();
-  
-  return (
-    <Tabs
-      screenOptions={{
-        headerShown: true, 
-        tabBarActiveTintColor: colors.primary[500], 
-        tabBarInactiveTintColor: colors.black[200],
-        tabBarStyle: {
-          backgroundColor: colors.surface.primary,
-          borderTopColor: colors.surface.tertiary,
-        },
-        headerStyle: {
-          backgroundColor: colors.surface.primary,
-        },
-        headerTintColor: colors.text.primary,
-        headerShadowVisible: false,
-      }}
-    >
-      <Tabs.Screen
-        name="map"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="navigate" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="walks"
-        options={{
-          title: 'Activity',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="filter" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: 'Profile',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+    const { colors } = useTheme();
+
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: colors.primary[500],
+                tabBarInactiveTintColor: colors.black[200],
+                tabBarStyle: {
+                    backgroundColor: colors.surface.primary,
+                    borderTopColor: colors.surface.tertiary,
+                },
+                headerStyle: {
+                    backgroundColor: colors.surface.primary,
+                },
+                headerTintColor: colors.text.primary,
+                headerShadowVisible: false,
+            }}
+        >
+            <Tabs.Screen
+                name="map"
+                options={{
+                    title: 'Explore',
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="navigate" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="walks"
+                options={{
+                    title: 'Activity',
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="filter" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: 'Profile',
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="person" color={color} size={size} />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
 }
 
 export default function Layout() {
-  return (
-    <ThemeProvider>
-      <TabsLayout />
-    </ThemeProvider>
-  );
+    return (
+        <TabsLayout />
+    );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,63 +1,62 @@
 import { Stack, router } from "expo-router";
 import "./globals.css";
 import { useFonts } from "expo-font";
-import { Suspense, use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import { supabase } from "@/libs/supabase";
-
+import { ThemeProvider } from "@/context/ThemeContext";
 
 export default function RootLayout() {
-  const [fontsLoaded] = useFonts({
-    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
-    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
-    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
-    "Rubrik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
-    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
-    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
-  });
-
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
-
-useEffect(() => {
-    if (fontsLoaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  useEffect(() => {
-
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setIsAuthenticated(!!session);
-  
-      if (!session) {
-        router.replace("/sign-in");
-      }
+    const [fontsLoaded] = useFonts({
+        "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+        "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+        "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+        "Rubrik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+        "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+        "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
     });
-  
-    const { data: { subscription } } = supabase.auth.onAuthStateChange(
-      (_event, session) => {
-        setIsAuthenticated(!!session);
-  
-        if (!session) {
-          router.replace("/sign-in");
-        } else {
-          router.replace("/(roots)/(tabs)/map");
+
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+
+    useEffect(() => {
+        if (fontsLoaded) {
+            SplashScreen.hideAsync();
         }
-      }
+    }, [fontsLoaded]);
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setIsAuthenticated(!!session);
+
+            if (!session) {
+                router.replace("/sign-in");
+            }
+        });
+
+        const { data: { subscription } } = supabase.auth.onAuthStateChange(
+            (_event, session) => {
+                setIsAuthenticated(!!session);
+
+                if (!session) {
+                    router.replace("/sign-in");
+                } else {
+                    router.replace("/(roots)/(tabs)/map");
+                }
+            }
+        );
+
+        return () => {
+            subscription.unsubscribe();
+        };
+    }, []);
+
+    if (!fontsLoaded || isAuthenticated === null) {
+        return null;
+    }
+
+    return (
+        <ThemeProvider>
+            <Stack screenOptions={{ headerShown: false }} />
+        </ThemeProvider>
     );
-  
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, []);
-  
-  if (!fontsLoaded||!isAuthenticated === null) {
-    return null; 
-  }
-  
-  return <Stack screenOptions ={{headerShown: false}}/>;
 }
-
-
-
-
