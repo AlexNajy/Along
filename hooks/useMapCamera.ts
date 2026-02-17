5bd5b4910f0a9078f649be0d4e90c92e2a1ab1fd
Alexander Najy <Alexander.najy@gmail.com>
2026-02-16 20:54:52 -0800

    zoom matches route size

--- hooks/useMapCamera.ts
+++ hooks/useMapCamera.ts
@@ -23,13 +23,24 @@ export const useMapCamera = (initialCenter: [number, number]) => {
         const maxLat = Math.max(...lats);
         const center: [number, number] = [(minLng + maxLng) / 2, (minLat + maxLat) / 2];
 
-        setCameraState({ center, zoom: 15, pitch: 30 });
+        const lngDiff = maxLng - minLng;
+        const latDiff = maxLat - minLat;
+        const maxDiff = Math.max(lngDiff, latDiff);
+        
+        let zoom = 15;
+        if (maxDiff > 0.05) zoom = 12;      
+        else if (maxDiff > 0.02) zoom = 13; 
+        else if (maxDiff > 0.01) zoom = 14; 
+        else if (maxDiff > 0.005) zoom = 15; 
+        else zoom = 16;                      
+
+        setCameraState({ center, zoom, pitch: 30 });
 
         cameraRef.current?.setCamera({
             bounds: {
                 ne: [maxLng, maxLat],
                 sw: [minLng, minLat],
-                paddingTop: 80,
+                paddingTop: 160,
                 paddingRight: 60,
                 paddingBottom: 0,
                 paddingLeft: 60,
